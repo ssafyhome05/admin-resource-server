aa825eea3f807947f3e4e89166622b20765442ba
s12171934 <ssong99999@naver.com>
2024-11-14 17:38:29 +0900

    [FEATURE] init

---
+++ src/test/java/com/oauth2admin/Oauth2adminResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2adminResourceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
